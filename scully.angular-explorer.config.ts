c68057c77c0cad8171c913f248b456b5ad82478d
ludo loops <ludoloops@protonmail.com>
2021-05-20 00:44:47 +0200

    Fix: scully config

--- scully.angular-explorer.config.ts
+++ scully.angular-explorer.config.ts
@@ -1,8 +1,7 @@
 import { ScullyConfig } from '@scullyio/scully';
 export const config: ScullyConfig = {
-  projectRoot: "./src",
-  projectName: "angular-explorer",
+  projectRoot: './src',
+  projectName: 'angular-explorer',
   outDir: './dist/static',
-  routes: {
-  }
+  routes: {},
 };
